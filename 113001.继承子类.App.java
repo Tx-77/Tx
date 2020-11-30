5c4c9bbae8db828ee85cbdd13b73a4f2e5c27d50
Tx-77 <74789194+Tx-77@users.noreply.github.com>
2020-11-30 15:27:49 +0800

    Update 113001.继承子类.App.java

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -4,8 +4,11 @@ public class App extends R{
 	public static void main(String[] args) {
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		System.out.println(array2d[1][1]);
-
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[0].length; j++) {
+				System.out.print(array2d[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
-
 }
