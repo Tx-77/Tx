36859bef0eeff9796f043999a72006cd86a26d67
Tx-77 <74789194+Tx-77@users.noreply.github.com>
2020-11-30 15:01:30 +0800

    Create  113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,11 @@
+
+public class App extends R{
+
+	public static void main(String[] args) {
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d[1][1]);
+
+	}
+
+}
